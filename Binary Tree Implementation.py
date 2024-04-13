ac8042ee42d392b3954dd6993c598abd7ffd8766
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-13 08:25:55 +0530

    Add files via upload

---
+++ Binary Tree Implementation.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+root=Node('R')
+nodeA=Node('A')
+nodeB=Node('B')
+nodeC=Node('C')
+nodeD=Node('D')
+nodeE=Node('E')
+nodeF=Node('F')
+nodeE=Node('E')
+nodeG=Node('G')
+
+
+root.left=nodeA
+root.right=nodeB
+
+nodeA.left=nodeC
+nodeA.right=nodeD
+
+nodeB.left=nodeE
+nodeB.right=nodeF
+
+nodeF.left=nodeG
+
+print(root.left.right.data)
