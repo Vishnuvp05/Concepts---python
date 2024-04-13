4564341d05f26ff973223665b0fe86d2789f1d39
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-13 08:46:12 +0530

    Add files via upload

---
+++ Pre-Order Traversal.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+#It is a type of Depth-First-Search where root node is visited and recursive traversal for left subtree and right subtree is done
+#root node is visited before recursive left subtree and right subtree hence it is called pre order 
+
+
+def preordertraversal(node):
+    if node is None:
+        return 
+    print(node.data,end=", ")
+    preordertraversal(node.left)
+    preordertraversal(node.right)
+
+root=Node('R')
+nodeA=Node('A')
+nodeB=Node('B')
+nodeC=Node('C')
+nodeD=Node('D')
+nodeE=Node('E')
+nodeF=Node('F')
+nodeG=Node('G')
+
+
+root.left=nodeA
+root.right=nodeB
+
+nodeA.left=nodeC
+nodeA.right=nodeD
+
+nodeB.left=nodeE
+nodeB.right=nodeF
+
+nodeF.left=nodeG
+
+preordertraversal(root)
