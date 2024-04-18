d57cbf7abfab5b23717ee1a3b49378aabb11b0cf
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-18 20:36:11 +0530

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -57,7 +57,7 @@ arr=[2,3,5,1,4,5,32]
 for i in range(1,len(arr)):
     j=i
     while arr[j-1]>arr[j] and j>0:
-        arr[j-1],arr[j+1]=arr[j],arr[j-1]
+        arr[j-1],arr[j]=arr[j],arr[j-1]
         j-=1
 print(arr)
 
